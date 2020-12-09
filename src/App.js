870fa5ec03bf18aec79b86cf1695084d3046184d
Juan Domínguez <juana.dominguez@softtek.com>
2020-12-08 19:04:29 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://assets.stickpng.com/images/58429073a6515b1e0ad75abc.png" />
+        <p>
+          Hello, this is a test to deploy in server
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
